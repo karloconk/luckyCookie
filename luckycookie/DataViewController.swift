bc27b4f52098aa819462f0e38818ccbed374bdea
Karlo <808khz@gmail.com>
2020-02-12 22:00:35 -0600

    added some animations

--- luckycookie/DataViewController.swift
+++ luckycookie/DataViewController.swift
@@ -16,23 +16,35 @@ class DataViewController: UIViewController {
     @IBOutlet weak var luckyButton: UIButton!
     @IBOutlet weak var popImage: UIImageView!
     @IBOutlet weak var topSeparator: UIImageView!
+    @IBOutlet weak var luckyLabel: UILabel!
+    @IBOutlet weak var luckyImage: UIImageView!
+    @IBOutlet weak var fondo: UIView!
+    @IBOutlet weak var bottomLabel: UILabel!
     
     //MARK:- Vars
     var currentTitle: String = "Basic"
     var index: Int?
-    let topSeparatorHideSecs = 1.0
+    let topSeparatorHideSecs = 0.25
+    var touched = false
 
     //MARK:- Actions
     
     @IBAction func buttonTapped(_ sender: Any) {
-        popCookie()
-        hideTopSeparator()
+        if touched {
+            reset()
+        } else {
+            popCookie()
+            hideTopSeparator()
+            showMessage()
+        }
     }
     
     //MARK:- Lifecycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.fondo.alpha        = 0.0
+        self.topSeparator.alpha = 1.0
         if self.index ?? 0 < 1 {
             setBackground(image: UIImage(named: "basic_fondo")!)
         }
@@ -46,9 +58,9 @@ class DataViewController: UIViewController {
     }
     
     func hideTopSeparator() {
-        DispatchQueue.main.asyncAfter(deadline: .now() + topSeparatorHideSecs) {
-            self.topSeparator.isHidden = true
-        }
+        UIView.animate(withDuration: 0.8, delay: 0.25, options: .curveLinear, animations: {
+            self.topSeparator.alpha = 0.0
+        }, completion: nil)
     }
     
     func popCookie() {
@@ -56,4 +68,31 @@ class DataViewController: UIViewController {
         self.popImage.isHidden = false
     }
 
+    func showMessage() {
+        chooseMessage()
+        UIView.animate(withDuration: 0.8, delay: 0.25, options: .curveLinear, animations: {
+            self.fondo.alpha = 1.0
+        }, completion: nil)
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.8 + 0.25) {
+            self.touched = true
+            self.bottomLabel.text = "Try again"
+
+        }
+    }
+    
+    func chooseMessage() {
+        luckyLabel.text = "KKDPRRO"
+    }
+    
+    func reset() {
+        self.fondo.alpha = 0.0
+        self.popImage.isHidden = true
+        UIView.animate(withDuration: 0.8, delay: 0.0, options: .curveLinear, animations: {
+            self.topSeparator.alpha = 1.0
+        }, completion: nil)
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.8) {
+            self.touched = false
+            self.bottomLabel.text = "Get Lucky"
+        }
+    }
 }
